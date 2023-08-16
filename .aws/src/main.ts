17d733e89ef6b69eb51a0d1632bdc01b8fa886fb
Katerina Chinnappan <kchinnappan@mozilla.com>
2023-08-15 17:22:09 -0700

    fixing tf

--- .aws/src/main.ts
+++ .aws/src/main.ts
@@ -151,7 +151,19 @@ class FirefoxAndroidHomeRecommendations extends TerraformStack {
             effect: 'Allow',
           },
         ],
-        taskRolePolicyStatements: [{}],
+        taskRolePolicyStatements: [
+          {
+            actions: [
+              'xray:PutTraceSegments',
+              'xray:PutTelemetryRecords',
+              'xray:GetSamplingRules',
+              'xray:GetSamplingTargets',
+              'xray:GetSamplingStatisticSummaries',
+            ],
+            resources: ['*'],
+            effect: 'Allow',
+          },
+        ],
         taskExecutionDefaultAttachmentArn:
           'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy',
       },
