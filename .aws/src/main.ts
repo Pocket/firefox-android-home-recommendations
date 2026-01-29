5b5a0937abb50b32110738bd33d35ad9bb49358e
Jonathan Petto <jpetto@users.noreply.github.com>
2026-01-29 12:19:17 -0600

    chore(infra): tear down service
    - pocket decommission
    - HNT-1628

--- .aws/src/main.ts
+++ .aws/src/main.ts
@@ -1,20 +1,9 @@
 import { Construct } from 'constructs';
 import { App, S3Backend, TerraformStack } from 'cdktf';
-import { PagerdutyProvider } from '@cdktf/provider-pagerduty/lib/provider';
 import { LocalProvider } from '@cdktf/provider-local/lib/provider';
 import { NullProvider } from '@cdktf/provider-null/lib/provider';
 import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
-import { DataAwsKmsAlias } from '@cdktf/provider-aws/lib/data-aws-kms-alias';
-import { DataAwsSnsTopic } from '@cdktf/provider-aws/lib/data-aws-sns-topic';
-import { DataAwsRegion } from '@cdktf/provider-aws/lib/data-aws-region';
-import { DataAwsCallerIdentity } from '@cdktf/provider-aws/lib/data-aws-caller-identity';
 import { config } from './config';
-import {
-  PocketALBApplication,
-  PocketPagerDuty,
-  PocketECSCodePipeline,
-} from '@pocket-tools/terraform-modules';
-import { CloudwatchLogGroup } from '@cdktf/provider-aws/lib/cloudwatch-log-group';
 
 class FirefoxAndroidHomeRecommendations extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -22,10 +11,6 @@ class FirefoxAndroidHomeRecommendations extends TerraformStack {
 
     new AwsProvider(this, 'aws', {
       region: 'us-east-1',
-    });
-
-    new PagerdutyProvider(this, 'pagerduty_provider', {
-      token: undefined,
     });
 
     new LocalProvider(this, 'local_provider');
@@ -37,167 +22,6 @@ class FirefoxAndroidHomeRecommendations extends TerraformStack {
       key: config.name,
       region: 'us-east-1',
     });
-
-    const pagerDuty = new PocketPagerDuty(this, 'pagerduty', {
-      prefix: config.prefix,
-      service: {
-        // This is a Tier 2 service and as such only raises non-critical alarms.
-        criticalEscalationPolicyId: 'PXOQVEP',
-        nonCriticalEscalationPolicyId: 'PXOQVEP',
-      },
-    });
-
-    const region = new DataAwsRegion(this, 'region');
-    const caller = new DataAwsCallerIdentity(this, 'caller');
-    const secretsManager = new DataAwsKmsAlias(this, 'kms_alias', {
-      name: 'alias/aws/secretsmanager',
-    });
-
-    const snsTopic = new DataAwsSnsTopic(this, 'backend_notifications', {
-      name: `Backend-${config.environment}-ChatBot`,
-    });
-
-    const albApplication = new PocketALBApplication(this, 'application', {
-      internal: false,
-      prefix: config.prefix,
-      alb6CharacterPrefix: config.shortName,
-      tags: config.tags,
-      cdn: true,
-      domain: config.domain,
-      containerConfigs: [
-        {
-          name: 'app',
-          portMappings: [
-            {
-              hostPort: 4005,
-              containerPort: 4005,
-            },
-          ],
-          envVars: [
-            {
-              name: 'ENVIRONMENT',
-              value: process.env.NODE_ENV, // this gives us a nice lowercase production and development
-            },
-          ],
-          logGroup: this.createCustomLogGroup('app'),
-          logMultilinePattern: '^\\S.+',
-          secretEnvVars: [
-            {
-              name: 'SENTRY_DSN',
-              valueFrom: `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}/SENTRY_DSN`,
-            },
-          ],
-          healthCheck: config.healthCheck,
-        },
-      ],
-      codeDeploy: {
-        useCodeDeploy: true,
-        useCodePipeline: true,
-        snsNotificationTopicArn: snsTopic.arn,
-        notifications: {
-          //only notify on failed deploys
-          notifyOnFailed: true,
-          notifyOnStarted: false,
-          notifyOnSucceeded: false,
-        },
-      },
-      exposedContainer: {
-        name: 'app',
-        port: 4005,
-        healthCheckPath: '/.well-known/server-health',
-      },
-      ecsIamConfig: {
-        prefix: config.prefix,
-        taskExecutionRolePolicyStatements: [
-          //This policy could probably go in the shared module in the future.
-          {
-            actions: ['secretsmanager:GetSecretValue', 'kms:Decrypt'],
-            resources: [
-              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared`,
-              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared/*`,
-              secretsManager.targetKeyArn,
-            ],
-            effect: 'Allow',
-          },
-          //This policy could probably go in the shared module in the future.
-          {
-            actions: ['ssm:GetParameter*'],
-            resources: [
-              `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}`,
-              `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}/*`,
-            ],
-            effect: 'Allow',
-          },
-        ],
-        taskRolePolicyStatements: [
-          {
-            actions: [
-              'xray:PutTraceSegments',
-              'xray:PutTelemetryRecords',
-              'xray:GetSamplingRules',
-              'xray:GetSamplingTargets',
-              'xray:GetSamplingStatisticSummaries',
-            ],
-            resources: ['*'],
-            effect: 'Allow',
-          },
-        ],
-        taskExecutionDefaultAttachmentArn:
-          'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy',
-      },
-      autoscalingConfig: {
-        targetMinCapacity: 2,
-        targetMaxCapacity: 10,
-      },
-      alarms: {
-        // alarms if >= 25% of responses are 5xx responses for 20 minutes
-        http5xxErrorPercentage: {
-          threshold: 25, // 25%
-          evaluationPeriods: 4,
-          period: 300, // 5 minutes
-          // non-critical while we investigate current 5xx responses
-          actions: config.isDev ? [] : [pagerDuty.snsNonCriticalAlarmTopic.arn],
-        },
-      },
-    });
-
-    this.createApplicationCodePipeline(albApplication);
-  }
-
-  /**
-   * Create CodePipeline to build and deploy terraform and ecs
-   * @param app
-   * @private
-   */
-  private createApplicationCodePipeline(app: PocketALBApplication) {
-    new PocketECSCodePipeline(this, 'code-pipeline', {
-      prefix: config.prefix,
-      source: {
-        codeStarConnectionArn: config.codePipeline.githubConnectionArn,
-        repository: config.codePipeline.repository,
-        branchName: config.codePipeline.branch,
-      },
-    });
-  }
-
-  /**
-   * Create Custom log group for ECS to share across task revisions
-   * @param containerName
-   * @private
-   */
-  private createCustomLogGroup(containerName: string) {
-    const logGroup = new CloudwatchLogGroup(
-      this,
-      `${containerName}-log-group`,
-      {
-        name: `/Backend/${config.prefix}/ecs/${containerName}`,
-        retentionInDays: 90,
-        skipDestroy: true,
-        tags: config.tags,
-      },
-    );
-
-    return logGroup.name;
   }
 }
 
